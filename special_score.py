b65270c410fab794de1165447d8201a6b3ecd4bd
iamohmcub <phakorn.s@x-gaming.com>
2024-03-27 00:19:57 +0700

    SPECIAL SCORE!!!

--- special_score.py
+++ special_score.py
@@ -3,3 +3,21 @@
 ข้อนี้ไม่ต้องทำก็ได้ ไม่มีผลลบกับการให้คะแนน แต่ถ้าทำมาเเล้วเจ๋งจริง ก็จะพิจารณาเป็นพิเศษ
 
 """
+input = 1040320
+
+# เป็นลอจิกเกี่ยวกับการถอนเงินที่ตู้ ATM 
+# เหตุเกิดจากความสงสัยเวลาเราไปถอนเงิน 
+# ตู้ ATM จะรู้ได้อย่างไรว่าต้องให้แบงค์อะไรบ้าง
+
+def bankWithdraw(input):
+    availableBank = [1000, 500, 100, 50, 20]
+    result = {}
+   
+    for bank in availableBank:
+        count = input // bank
+        input %= bank
+        result[f"แบงค์ {bank}"] = count
+    return result
+
+output = bankWithdraw(input)
+print(output)
